04e69f22eb3413e4bd1060d60455208d7dafc76e
haiyuepu <haiyuepu032@gmail.com>
2024-07-07 19:37:02 +0800

    add api.py add PDFLoader.py add TXTLoader.py

--- api.py
+++ api.py
@@ -4,12 +4,15 @@ os.environ['https_proxy'] = "http://127.0.0.1:8890"
 
 from openai import OpenAI
 
-client = OpenAI(api_key="sk-proj-ESRo7hcbf5TUZ2qvbpikT3BlbkFJ0geARDwJLnIo5P3DAWIr")
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=api_key)
+
 
 completion=client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role":"user","content":"你好，请你介绍一下你自己."}
-    ]
-)
+     model="gpt-3.5-turbo",
+     messages=[
+         {"role":"user","content":"你好，请你介绍一下你自己."}
+     ]
+ )
 print(completion.choices[0].message.content)
+
