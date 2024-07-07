b5e488638014720340e79596a77612272a9b41d6
Amino Acid <13392129373@163.com>
2024-07-07 18:37:01 +0800

    add prompt_config.py

--- configs/prompt_config.py
+++ configs/prompt_config.py
@@ -5,7 +5,7 @@ prompts['llm_chat'] = 'You are an assistant to answer problem from human. \n {{i
 
 prompts['kb_chat'] = '''
     Based on the content in the knowledge base, answer the {{question}} using plain language. 
-    If the answer can not be answer from knowledge base, say 'Sorry, we don't know enough 
+    If the answer can not be answered from knowledge base, say 'Sorry, I don't know enough 
     to answer that question.' You are not allowed to add additional information that is not 
     in the knowledge base into the answer.
     The language used should be same as the {{question}}.
