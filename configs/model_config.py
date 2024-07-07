8a9bdfc612b06c083d54d01d8600076fc213a2ef
Amino Acid <13392129373@163.com>
2024-07-07 14:08:28 +0800

    add model_config.py

--- configs/model_config.py
+++ configs/model_config.py
@@ -1,3 +1,27 @@
 import os
 
 model_root_path = ''
+
+model_path = {
+    'embed_model' : {
+        'text-embedding-3-small' : os.getenv('API_KEY')
+    },
+    'llm_model' : {
+        'GPT-3.5-turbo': os.getenv('API_KEY')
+    }
+}
+
+embedding_model = 'text-embedding-3-small'
+embedding_device = 'auto'
+
+llm_model = 'GPT-3.5-turbo'
+llm_device = 'auto'
+
+len_history = 8
+temperature = 0.5
+
+langchain_llm_model = {
+    'OpenAI' : {
+
+    }
+}
