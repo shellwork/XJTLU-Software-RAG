09c5e3e84860c10734689b51dd649173fe98b0a2
Amino Acid <13392129373@163.com>
2024-07-07 15:23:48 +0800

    add model_config.py

--- configs/model_config.py
+++ configs/model_config.py
@@ -1,7 +1,6 @@
 import os
 
 model_root_path = ''
-
 model_path = {
     'embed_model' : {
         'text-embedding-3-small' : os.getenv('API_KEY')
@@ -22,6 +21,11 @@ temperature = 0.5
 
 langchain_llm_model = {
     'OpenAI' : {
-
+        'model_name' : 'GPT-3.5-turbo',
+        'api_base_url': 'https://api.openai.com/v1',
+        'api_key' : os.getenv('API_KEY'),
+        'openai_proxy': ''
     }
 }
+
+nltk_data_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "nltk_data")
